94ef0b067b6f787bdaff01396c03047b9bb849dd
pallavghoshal <ghoshalpallav@gmail.com>
2022-06-23 16:13:48 +0530

    Memory Game(Yadasht)

---
+++ MyMemory/app/src/main/java/com/pallav/mymemory/utils/Constants.kt
@@ -0,0 +1,20 @@
+package com.pallav.mymemory.utils
+
+import com.pallav.mymemory.R
+
+const val EXTRA_GAME_NAME = "EXTRA_BOARD_SIZE"
+const val EXTRA_BOARD_SIZE = "EXTRA_BOARD_SIZE"
+val DEFAULT_ICONS = listOf(
+    R.drawable.ic_face,
+    R.drawable.ic_flight,
+    R.drawable.ic_flower,
+    R.drawable.ic_giftcard,
+    R.drawable.ic_heart,
+    R.drawable.ic_home,
+    R.drawable.ic_lightning,
+    R.drawable.ic_moon,
+    R.drawable.ic_school,
+    R.drawable.ic_send,
+    R.drawable.ic_star,
+    R.drawable.ic_work
+)
